9756b22c2406445c765d2b5f8c79bade190c6d5c
Robert Lowry <bobertlo@gmail.com>
2022-05-22 21:53:01 -0500

    add ensemble support to train.py, train a few more epochs

--- train.py
+++ train.py
@@ -5,11 +5,15 @@ from tensorflow.keras import regularizers
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.metrics import AUC
 
+from sklearn.model_selection import KFold
+
 from sklearn.model_selection import train_test_split
 
 from dvclive.keras import DvcLiveCallback
 from ruamel.yaml import YAML
 import pickle
+import json
+import os
 
 # load the parameter file and select the 'train' section:
 yaml = YAML(typ='safe')
@@ -21,16 +25,14 @@ print("Parameters:", params)
 # load the training/validation datasets
 print("laoding dataset")
 with open('train.pickle', 'rb') as data_file:
-    (x_train, y_train) = pickle.load(data_file)
-print(x_train.shape, y_train.shape)
-
-x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train)
+    (X_train, y_train) = pickle.load(data_file)
+print(X_train.shape, y_train.shape)
 
 def make_model():
     model = Sequential()
     model.add(Dense(512, 
         kernel_regularizer=regularizers.l2(1e-5),
-        input_dim=x_train.shape[1], activation="swish"))
+        input_dim=X_train.shape[1], activation="swish"))
     model.add(Dropout(params.get("dropout", 0)))
     model.add(Dense(256, 
         kernel_regularizer=regularizers.l2(1e-5),
@@ -47,17 +49,51 @@ def make_model():
     model.add(Dense(1, activation="sigmoid"))
     return model
 
-model = make_model()
 
-opt = Adam(learning_rate = params.get("lr", 0.01))
-lf = BinaryCrossentropy()
-metrics = [AUC(name="auc")]
-model.compile(optimizer=opt, loss=lf, metrics=metrics)
-model.fit(
-    x_train, y_train,
-    validation_data=(x_valid, y_valid),
-    batch_size = params.get("bs", 2048),
-    epochs=params.get("epochs", 10),
-    callbacks=[DvcLiveCallback(path="training_metrics")])
+def train_model(X_train, X_valid, y_train, y_valid, callbacks=[]):
+    model = make_model()
+    opt = Adam(learning_rate = params.get("lr", 0.01))
+    lf = BinaryCrossentropy()
+    metrics = [AUC(name="auc")]
+    model.compile(optimizer=opt, loss=lf, metrics=metrics)
+    history = model.fit(
+        X_train, y_train,
+        validation_data=(X_valid, y_valid),
+        batch_size = params.get("bs", 2048),
+        epochs=params.get("epochs", 10),
+        callbacks=callbacks)
+    metrics = {}
+    metrics['auc'] = history.history['auc'][-1]
+    metrics['val_auc'] = history.history['val_auc'][-1]
+    metrics['loss'] = history.history['loss'][-1]
+    metrics['val_loss'] = history.history['val_loss'][-1]
 
-model.save("model")
+    return model, metrics
+
+if params.get("ensemble", False):
+    kf = KFold(n_splits=5)
+    metrics = []
+    for fold, (idx_tr, idx_va) in enumerate(kf.split(X_train)):
+        X_tr = X_train.iloc[idx_tr]
+        X_va = X_train.iloc[idx_va]
+        y_tr = y_train.iloc[idx_tr]
+        y_va = y_train.iloc[idx_va]
+
+        fold_model, fold_metrics = train_model(X_tr, X_va, y_tr, y_va)
+        fold_model.save("model/fold_" + str(fold))
+        metrics.append(fold_metrics)
+    mean_metrics = {}
+    for key in metrics[0].keys():
+        key_metrics = []
+        for m in metrics:
+            key_metrics.append(m[key])
+        mean_metrics[key] = sum(key_metrics) / len(key_metrics)
+    with open("training_metrics.json", "w") as json_file:
+        json.dump(mean_metrics, json_file)
+    # dvc is looking for this from dvclive in single run mode...
+    os.makedirs("training_metrics/scalars", exist_ok=True)
+
+else:
+    X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train)
+    model, _ = train_model(X_train, X_valid, y_train, y_valid, callbacks=[DvcLiveCallback(path="training_metrics")])
+    model.save("model")
